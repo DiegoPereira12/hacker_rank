87dae9a3eea9e7cafcb2142a909514b2172905ce
Diego Pereira <diegoo.junior55@gmail.com>
2021-10-13 22:06:25 -0300

    nova atividade realizada

---
+++ arithmetic_operators.py
@@ -0,0 +1,33 @@
+'''
+Task
+The provided code stub reads two integers from STDIN,  a and b. Add code to print three lines where:
+
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers.
+
+Example
+a = 3
+b = 5
+
+Print the following:
+
+8
+-2
+15
+
+'''
+
+if __name__ == '__main__' :
+
+    a = 3 
+    b = 2
+
+    soma = a + b
+    print(soma)
+
+    diminuir = a - b
+    print(diminuir)
+
+    multiplicar = a * b
+    print(multiplicar)
--- pyhton_if_else.py
+++ pyhton_if_else.py
@@ -2,23 +2,20 @@
 Task
 Given an integer, , perform the following conditional actions:
 
-If  is odd, print Weird
-If  is even and in the inclusive range of  to , print Not Weird
-If  is even and in the inclusive range of  to , print Weird
-If  is even and greater than , print Not Weird
+If n is odd, print Weird
+If n is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive range of 6 to 20, print Weird
+If n is even and greater than 20, print Not Weird
 '''
 
-import sys
+n = int(input('Digite um numero => '))
 
-
-N = int(input().strip())
-
-if N % 2 != 0:
+if n % 2 != 0:
     print ("Weird")
 else:
-    if N >= 2 and N <= 5:
+    if n >= 2 and n <= 5:
         print ("Not Weird")
-    elif N >= 6 and N <= 20:
+    elif n >= 6 and n <= 20:
         print ("Weird")
-    elif N > 20:
+    elif n > 20:
         print ("Not Weird")
